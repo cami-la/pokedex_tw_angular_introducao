ece57544d06b6426298c22f5d67978b643eaef38
Camila Cavalcante <camiladsantoscavalcante@gmail.com>
2022-05-01 21:15:28 -0300

    first commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { ListComponent } from './views/list/list.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ListComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
